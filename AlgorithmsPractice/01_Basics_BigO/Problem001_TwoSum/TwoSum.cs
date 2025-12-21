b3a94da1ccf479b94fad4fb5bb9dd74736a6f0fd
Uzair Iqbal <uzair.qq@outlook.com>
2025-12-21 08:56:32 +0500

    Two Sum: simple nested-loops solution, stop after first match

---
+++ AlgorithmsPractice/01_Basics_BigO/Problem001_TwoSum/TwoSum.cs
@@ -0,0 +1,31 @@
+﻿
+namespace AlgorithmsPractice._01_Basics_BigO.Problem001_TwoSum
+{
+
+    public static class TwoSum
+    {
+        public static List<int> CheckTwoSum(int target, int[] arr)
+        {
+            var list = new List<int>();
+            var flag = false;
+           
+
+            for (var i = 0; i < arr.Length; i++)
+            {
+                for (var j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] + arr[j] == target)
+                    {
+                        flag = true;
+                        list.Add(i);
+                        list.Add(j);
+                        break;
+                    }
+                }
+                if (flag == true)
+                    break;
+            }
+            return list;
+        }
+    }
+}
