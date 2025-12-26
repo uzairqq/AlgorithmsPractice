268d3d8035395319f6c732e15a9981aa605e102a
Uzair Iqbal <uzair.qq@outlook.com>
2025-12-26 12:49:01 +0500

    Add Problem001 - TwoSum brute force solution with test cases

--- AlgorithmsPractice/01_Basics_BigO/Problem001_TwoSum/TwoSum.cs
+++ AlgorithmsPractice/01_Basics_BigO/Problem001_TwoSum/TwoSum.cs
@@ -1,31 +1,22 @@
-﻿
 namespace AlgorithmsPractice._01_Basics_BigO.Problem001_TwoSum
 {
-
     public static class TwoSum
     {
-        public static List<int> CheckTwoSum(int target, int[] arr)
+        public static int[] TwoSumCheck(int[] arr, int target)
         {
-            var list = new List<int>();
-            var flag = false;
-           
-
             for (var i = 0; i < arr.Length; i++)
             {
                 for (var j = i + 1; j < arr.Length; j++)
                 {
                     if (arr[i] + arr[j] == target)
                     {
-                        flag = true;
-                        list.Add(i);
-                        list.Add(j);
-                        break;
+                        return [i, j];
                     }
                 }
-                if (flag == true)
-                    break;
             }
-            return list;
+            return Array.Empty<int>();
         }
+
     }
 }
+
--- AlgorithmsPractice/Program.cs
+++ AlgorithmsPractice/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace AlgorithmsPractice
+﻿using AlgorithmsPractice._01_Basics_BigO.Problem001_TwoSum;
+
+namespace AlgorithmsPractice
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TwoSum.TwoSumCheck([2, 7, 11, 15], 9);   // expect [0,1]
+            TwoSum.TwoSumCheck([3, 2, 4], 6);        // expect [1,2]
+            TwoSum.TwoSumCheck([3, 3], 6);           // expect [0,1]
+            TwoSum.TwoSumCheck([1, 2, 3], 6);        // expect no pair
         }
     }
 }
